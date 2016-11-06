ac1d6b08a44a61699217741b8d993ab579e2e758
Giliam Meeussen <Giliam Meeussen>
2016-11-06 12:33:12 -0500

    Finalized sample project

--- Watershed Game/Assets/_Completed-Assets/Scripts/PlayerController.cs
+++ Watershed Game/Assets/_Completed-Assets/Scripts/PlayerController.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+    public float speed = 10;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
 }
