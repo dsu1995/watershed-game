9e230be733bc08edd6a2990220734c01a51b32f1
Jesse Jie <jesse_jie@hotmail.com>
2016-11-06 17:00:22 -0500

    Source and sink tiles

---
+++ Watershed Game/Assets/_Completed-Assets/Scripts/SinkTile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SinkTile : AbstractTile {
+
+	public SinkTile(uint x, uint y, TileMap map)
+		: base(x, y, map, float.MinValue)
+	{ }
+
+	public override float getPermeability()
+	{
+		return 1;
+	}
+
+	public override string getType()
+	{
+		return "sink";
+	}
+}
---
+++ Watershed Game/Assets/_Completed-Assets/Scripts/SourceTile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SourceTile : AbstractTile {
+
+	public SourceTile(uint x, uint y, TileMap map)
+		: base(x, y, map, float.MaxValue)
+	{ }
+
+	public override float getPermeability()
+	{
+		return 0;
+	}
+
+	public override string getType()
+	{
+		return "source";
+	}
+}
