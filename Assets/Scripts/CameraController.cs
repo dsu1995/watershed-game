b7a7f88d2ad7ac4d64ab74d2855324f0f75f2d0b
Kevin Xiao <kevinxiao2@gmail.com>
2016-11-11 17:40:02 -0500

    Add camera scroll and zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,17 +3,43 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-    public GameObject player;
+    public GameObject gameMap;
 
     private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
-        offset = transform.position - player.transform.position;
+        offset = new Vector3(5.5f, 5.5f, -1);
+
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-        transform.position = player.transform.position + offset;
-	}
+        if(Input.mousePosition.x <= 0)
+        {
+            offset.x -= 0.1f;
+        }
+        else if (Input.mousePosition.x >= Screen.width)
+        {
+            offset.x += 0.1f;
+        }
+        if (Input.mousePosition.y <= 0)
+        {
+            offset.y -= 0.1f;
+        }
+        else if (Input.mousePosition.y >= Screen.height)
+        {
+            offset.y += 0.1f;
+        }
+        transform.position = gameMap.transform.position + offset;
+        var scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll > 0f)
+        {
+            GetComponent<Camera>().orthographicSize -= 0.5f;
+        }
+        else if (scroll < 0f)
+        {
+            GetComponent<Camera>().orthographicSize += 0.5f;
+        }
+    }
 }
